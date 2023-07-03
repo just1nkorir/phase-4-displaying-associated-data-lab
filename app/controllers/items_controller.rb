8126ed86696d4b4f30ef44bd86095c37a9a653c5
just1nkorir <justin.korir@student.moringaschool.com>
2023-07-03 10:03:17 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Items.all 
+        render json: items, include: :users
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(user_params)
+        if user
+            render json: user, include: :items
+        render
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :city)
+    end
 end
